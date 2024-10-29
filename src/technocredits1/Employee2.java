d052c9a812332572e52c53c8cc17a52126abd390
Amardeep Jagdale <amardeepjagdale111@gmail.com>
2024-10-29 07:59:38 +0530

    added into master

---
+++ src/technocredits1/Employee2.java
@@ -0,0 +1,12 @@
+package technocredits1;
+
+public class Employee2 {
+
+
+	
+	void display() {
+		Employee1 emp1 = new Employee1();
+		emp1.readDataFromTxtFile();
+		System.out.println(emp1.num);
+	}
+}
